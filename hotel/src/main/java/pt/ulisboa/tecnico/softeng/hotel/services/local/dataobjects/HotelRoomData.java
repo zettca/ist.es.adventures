d90f82212d53aa588cdf266a5645ed486e6eaf29
João Dias <joao.spsd@gmail.com>
2017-05-07 00:55:26 +0100

    Fixed hotel

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/local/dataobjects/HotelRoomData.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/local/dataobjects/HotelRoomData.java
@@ -1,12 +1,16 @@
 package pt.ulisboa.tecnico.softeng.hotel.services.local.dataobjects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
 
 public class HotelRoomData {
 	
     private String number;
-    private pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type type;
+    private Type type;
+    private List<RoomBookingData> bookings = new ArrayList<>();
 
     public HotelRoomData() {
     }
@@ -31,4 +35,8 @@ public class HotelRoomData {
     public void setType(Type type) {
         this.type = type;
     }
+
+	public List<RoomBookingData> getBookings() {
+		return bookings;
+	}
 }
