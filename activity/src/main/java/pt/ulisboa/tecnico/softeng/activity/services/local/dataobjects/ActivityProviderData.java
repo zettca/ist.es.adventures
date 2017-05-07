e87dae825156c78bd76168d3e90c2946e339d996
Joana Pereira <joanaazambuja@gmail.com>
2017-05-07 12:03:13 +0100

    Added List of activities to ProviderData

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/dataobjects/ActivityProviderData.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/local/dataobjects/ActivityProviderData.java
@@ -1,52 +1,59 @@
 package pt.ulisboa.tecnico.softeng.activity.services.local.dataobjects;
 
+import java.util.List;
 import pt.ulisboa.tecnico.softeng.activity.domain.ActivityProvider;
 
 public class ActivityProviderData {
 	public enum CopyDepth {
 		SHALLOW, ACTIVITY, ACTIVITYOFFER, BOOKING
 	}
-	
+
 	private String name;
 	private String code;
-	
+	private List<ActivityData> activities;
+
 	public ActivityProviderData() {
 	}
-	
+
 	public ActivityProviderData(ActivityProvider activity, CopyDepth depth) {
 		this.name = activity.getName();
 		this.code = activity.getCode();
-		
+
 		switch(depth) {
-			case ACTIVITY:
-				//TODO
-				break;
-			case ACTIVITYOFFER:
-				//TODO
-				break;
-			case BOOKING:
-				//TODO
-				break;
-			case SHALLOW:
-				break;
-			default:
-				break;
+		case ACTIVITY:
+			//TODO
+			break;
+		case ACTIVITYOFFER:
+			//TODO
+			break;
+		case BOOKING:
+			//TODO
+			break;
+		case SHALLOW:
+			break;
+		default:
+			break;
 		}
 	}
-	
-	 public String getName() {
-		 return name;
-	 }
 
-	 public void setName(String name) {
-	     this.name = name;
-	 }
+	public String getName() {
+		return name;
+	}
 
-	 public String getCode() {
-	     return code;
-	 }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-	 public void setCode(String code) {
-		 this.code = code;
-	 }
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public List<ActivityData> getActivities() {
+		return activities;
+	}
 }
